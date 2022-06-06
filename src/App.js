5896b8085027c8a437809f6e4bbd3c94fef4fadd
Franco <francojvarela@gmail.com>
2022-06-06 13:58:51 -0300

    nuevo repo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './componentes/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          ¡Bienvenidos a mi App de React!
---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import "./navBar.css"
+
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav className="nav-main">
+        <ul>
+            <li>Link 1</li>
+            <li>link 2</li>
+            <li>Link 3</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
