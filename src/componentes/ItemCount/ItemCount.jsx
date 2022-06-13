eb0c578a62c1eadd7ec1ace6e852f3bc13cfe3e6
Franco <francojvarela@gmail.com>
2022-06-13 14:26:59 -0300

    nuevo repositorio

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import './itemCount.css'
 
 function ItemCount({stock, initial}) {
     const [count, setCount] = useState (initial);
@@ -10,7 +11,7 @@ function ItemCount({stock, initial}) {
         setCount(count -1)
     }
   return (
-    <div>
+    <div className='itemCount'>
         <button onClick={resta}>-</button>
         <span>{count}</span>
         <button onClick={suma}>+</button>
